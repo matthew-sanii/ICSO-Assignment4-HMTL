fc0e160ccedee1c7eb07908d2354fd3608446902
matthew.sanii <matthew.sanii@mths.ca>
2020-04-16 16:53:14 +0000

    edited extra.py

--- extra.py
+++ extra.py
@@ -1,7 +1,7 @@
 """
 Your module description
 """
-x = input()
-import math
-pi = math.pi
-y = 2 * x * pi
+PI = (3.14)
+r = float(input('Enter the radius of the circle :'))
+area = PI * r * r
+print("Area of the circle is : %.2f" %area)
