f87fca2b5611a42833fafb2907e677c1b6cb1706
matthew.sanii <matthew.sanii@mths.ca>
2020-04-16 17:27:08 +0000

    edited extra.py

--- extra.py
+++ extra.py
@@ -2,6 +2,15 @@
 Your module description
 """
 PI = (3.14)
-r = float(input('Enter the radius of the circle :'))
-area = PI * r * r
-print("Area of the circle is : %.2f" %area)
+while True:
+    diameter = input('Enter the diameter of the circle :')
+    try:
+        dia = float(diameter)
+    except ValueError:
+        print("Oops!  That was no valid number. Try again...")
+        break
+    r = dia / 2
+    area = PI * r * r
+    circumference = PI * r * 2
+    print("Area of the circle is : %.2f" %area)
+    print("Circumference of the circle is : %.2f" %circumference)
