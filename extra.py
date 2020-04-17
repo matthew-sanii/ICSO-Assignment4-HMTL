e8c33a58fd5a2ebb60936304f3bf99a96fcd6f93
matthew.sanii <matthew.sanii@mths.ca>
2020-04-17 23:55:51 +0000

    edited index.html

--- extra.py
+++ extra.py
@@ -9,8 +9,11 @@ while True:
   except ValueError:
     print("Oops!  That was no valid number. Try again...")
     break
-  r = dia / 2
-  area = PI * r * r
-  circumference = PI * r * 2
-  print("Area of the circle is : %.2f" %area)
-  print("Circumference of the circle is : %.2f" %circumference)
+  if dia <= 0:
+    print("value must be bigger than 0, please try again")
+  else:
+    r = dia / 2
+    area = PI * r * r
+    circumference = PI * r * 2
+    print("Area of the circle is : %.2f" %area)
+    print("Circumference of the circle is : %.2f" %circumference)
